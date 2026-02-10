530c4f6f3a701e7582a8236098ec1bde384090d3
ssmathoun <shabad.mathoun@gmail.com>
2026-02-10 11:51:30 -0500

    fix: use relative api paths

--- codesdev/client/vite.config.ts
+++ codesdev/client/vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
